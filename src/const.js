25aab3f0c3de0a3638571bde95d630350df82566
pusanukbr <pusanukbr@live.ru>
2022-04-01 00:39:23 +0300

    night

--- src/const.js
+++ src/const.js
@@ -1,7 +1,17 @@
 import ru from './ru';
+import en from './en';
+import ua from './ua';
+import i18n from 'i18next';
+
+const langWord = () => {
+  if(i18n.language === 'ru') return ru;
+  if(i18n.language === 'en') return en;
+  if(i18n.language === 'ua') return ua;
+}
 
 const wordsRandom  = () => {
-  return ru[Math.floor(Math.random()*ru.length)];
+  const word = langWord();
+  return word[Math.floor(Math.random() * word.length)];
 };
 
 
