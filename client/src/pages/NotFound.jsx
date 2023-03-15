2a1d20c5cdc726f90d1d2abcf7fc1e5ce9599384
pusanukbr <pusanukbr@live.ru>
2023-03-15 01:10:28 +0100

    add page 404

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import RouterConfig from '../const/RouterConfig';
 import { useTranslation } from 'react-i18next';
-import {
-  Box,
-  Container,
-  Stack,
-  useBreakpointValue,
-  useColorModeValue,
-  Button
-} from '@chakra-ui/react';
+import { Container, Button, Stack, Text } from '@chakra-ui/react';
 
 export default function NotFound() {
   const { t } = useTranslation();
@@ -17,7 +10,21 @@ export default function NotFound() {
   const fromPage = location.state?.from?.pathname || RouterConfig.LOBBY.path;
   return (
     <div>
-      <Link to={fromPage}>{t('page.notFound.back')}</Link>
+      <Container
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
+        justifyContent="center"
+        height="100vh">
+        <Stack fontSize="2xl" flexDirection="column" alignItems="center" mb="4">
+          <Text>{t('page.notFound.title')}</Text>
+          <Text>404</Text>
+        </Stack>
+
+        <Link to={fromPage}>
+          <Button>{t('page.notFound.backButton')}</Button>
+        </Link>
+      </Container>
     </div>
   );
 }
