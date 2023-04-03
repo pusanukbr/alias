6da91585e3d176193f56d4d5cb52cb040d7ac0b4
pusanukbr <pusanukbr@live.ru>
2023-04-03 03:01:40 +0200

    clear index

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import cookieParser from "cookie-parser";
 const app = express();
 import http from "http";
 const server = http.Server(app);
-// const io = require('socket.io')(server, { cors: { origin: '*' } });
 import cors from "cors";
 import { connect } from "mongoose";
 dotenv.config();
@@ -28,72 +27,6 @@ const startServer = async () => {
     console.log(e);
   }
 };
-// client
-// app.use(authRouter);
-
-// app.post('/login', async (req, res) => {
-//   try {
-//     const { userName, roomId, password } = req.body;
-
-//     const roomsHas = await Rooms.findOne({ roomId: roomId });
-//     const hashPassword = bcrypt.hashSync(password, 7)
-//     if(!roomsHas) {
-//       const roomsTest = new Rooms({
-//         roomId,
-//         users: [{
-//           userName,
-//         }],
-//         password: hashPassword,
-//       });
-//       await roomsTest.save();
-//       return req.json();
-//     }
-//     roomsHas.users.push({ userName: userName });
-//     await roomsHas.save();
-//     return req.json();
-//   } catch (e) {
-//       res.send({message: "Server error"})
-//   }
-//   // res.send({
-//   //   roomId,
-//   //   userName,
-//   //   test: 'test',
-//   // });
-// })
-
-// // socket
-// io.on('connection', (socket) => {
-//   console.log('connection');
-//   socket.on('ROOM:JOIN', async ({ roomId, userName }) => {
-//     socket.join(roomId);
-//     socket.to(roomId).emit('ROOM:SET_USERS');
-//     // test
-//     const roomsHas = await Rooms.findOne({ roomId: roomId });
-//     if(!roomsHas) {
-//       // const roomsTest = new Rooms({
-//       //   roomId,
-//       //   users: [{
-//       //     userName,
-//       //   }],
-//       // })
-//       // await roomsTest.save();
-//     } else {
-//       // roomsHas.users.push({userName: userName});
-//       // await roomsHas.save();
-//     }
-//   });
-
-//   socket.on('ROOM:NEW_MESSAGES', ({ roomId, userName, text }) => {
-//     const obj = {
-//       userName,
-//       text,
-//     }
-//     socket.to(roomId).emit('ROOM:SET_MESSAGES', obj);
-//   });
-//   socket.on('disconnect', () => {
-//     console.log('disconnect');
-//   })
-// })
 
 // server
 startServer();
