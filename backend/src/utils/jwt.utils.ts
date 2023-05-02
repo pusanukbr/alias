517e2f8c658ff92fbe8a47f31def81320bcfd53a
Bohdan Pusanuk <pusanukbr@gmail.com>
2023-05-02 02:06:03 +0200

    fix session created

--- backend/src/utils/jwt.utils.ts
+++ backend/src/utils/jwt.utils.ts
@@ -7,7 +7,6 @@ const publicKey = config.get<string>("publicKey");
 export function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
   return jwt.sign(object, privateKey, {
     ...(options && options),
-    algorithm: "RS256",
   });
 }
 
